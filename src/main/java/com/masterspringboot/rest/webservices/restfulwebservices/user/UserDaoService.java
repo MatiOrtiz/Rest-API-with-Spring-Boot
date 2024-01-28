1c6b52a8b44eb261a315f1cb81fbf47f32dd434d
MatiOrtiz <matiasnortiz.dev@gmail.com>
2024-01-28 15:30:31 -0300

    Post method for UserResource

--- src/main/java/com/masterspringboot/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/masterspringboot/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -10,12 +10,19 @@ import java.util.function.Predicate;
 @Component
 public class UserDaoService {
     private static List<User> users= new ArrayList<User>();
+    private static int usersCount= 0;
 
     static {
-        users.add(new User(1,"John", LocalDate.now().minusYears(20)));
-        users.add(new User(2,"Matt", LocalDate.now().minusYears(25)));
-        users.add(new User(3,"Liz", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount,"John", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount,"Matt", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount,"Liz", LocalDate.now().minusYears(30)));
 
+    }
+
+    public User save(User user){
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
     }
 
     public List<User> findAll(){
