d50879bc6f1dee79f56e1e1056ccc53763bd3d4a
Matías Nicolás Ortiz <88355010+MatiOrtiz@users.noreply.github.com>
2024-02-02 13:25:14 -0300

    connectin to h2

--- src/main/java/com/masterspringboot/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/masterspringboot/rest/webservices/restfulwebservices/user/User.java
@@ -1,13 +1,21 @@
 package com.masterspringboot.rest.webservices.restfulwebservices.user;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name= "user_details")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message = "Name should have atleast 2 characters")
     @JsonProperty("user_name")
@@ -20,6 +28,9 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
     }
 
     public Integer getId() {
