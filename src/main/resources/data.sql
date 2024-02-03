2092716cdee0c5d6d78782db9063098d5ff9b799
MatiOrtiz <matiasnortiz.dev@gmail.com>
2024-02-02 22:36:36 -0300

    Post entity with ManyToOne with UserEntity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,3 @@
-create table user_details
-(
-    id         INTEGER not null,
-    birth_date DATE    not null,
-    name       VARCHAR not null
-);
-
 insert into user_details(id, birth_date, name)
 values ( 10001, CURRENT_DATE(), 'Matias' );
 
@@ -13,3 +6,12 @@ values ( 10002, CURRENT_DATE(), 'Matt' );
 
 insert into user_details(id, birth_date, name)
 values ( 10003, CURRENT_DATE(), 'Mathew' );
+
+insert into post(id, description, user_id)
+values(20001, 'I learned Spring JPA', 10001);
+
+insert into post(id, description, user_id)
+values(20002, 'I learned Spring Framework', 10002);
+
+insert into post(id, description, user_id)
+values(20003, 'I learned Spring Bootcamp', 10003);
